56e1eddd4f545ea146e9e4e609c2bb4a8fbcd974
Deathshamhd <122151628+Deathshamhd@users.noreply.github.com>
2023-10-30 00:32:11 +0300

    save

--- src/components/phonebook.js
+++ src/components/phonebook.js
@@ -1,160 +1,158 @@
 import React, { Component } from 'react';
-import { nanoid } from 'nanoid'
-import{
-    MainContainer,
-    ContactsTable,
-    AddContactBttn,
-    ContactDetails,
-    ContactsContainer,
-    FindContact,
-    RemoveBttn
-} from './phonebook.styled'
+import { nanoid } from 'nanoid';
+import {
+  MainContainer,
+  ContactsTable,
+  AddContactBttn,
+  ContactDetails,
+  ContactsContainer,
+  FindContact,
+  RemoveBttn,
+} from './phonebook.styled';
 
-export class PhoneBook extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            contacts: [],
-            filter: '',
-            name: '',
-            number: '',
+class PhoneBook extends Component {
+  constructor(props) {
+    super(props);
+    // Initialize the component's state
+    this.state = {
+      contacts: [], // An array to store contact information
+      filter: '', // Used to filter contacts by name
+      name: '', // Name input field value
+      number: '', // Phone number input field value
+    };
+  }
+
+  handleChange = e => {
+    // Update the 'name' state when the name input changes
+    this.setState({
+      name: e.target.value,
+    });
+  };
+
+  handleNumberChange = e => {
+    // Update the 'number' state when the phone number input changes
+    this.setState({
+      number: e.target.value,
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { contacts, name, number } = this.state;
+
+    // Check if name and number are not empty
+    if (name.trim() && number.trim() !== '') {
+      if (contacts.some(contact => contact.name === name)) {
+        // Alert if a contact with the same name already exists
+        window.alert('Error: Contact already exists.');
+      } else {
+        // If contact doesn't exist, create a new contact object with a unique 'id'
+        const newContact = {
+          id: nanoid(), // Generate a unique ID for the contact
+          name: name, // Name of the contact
+          number: number, // Phone number of the contact
         };
-    }
-
-    handleChange = e => {
-        
-        this.setState({
-            name: e.target.value
-        });
-        
-        
-    };
-    handleNumberChange = e => {
-        this.setState({
-            number: e.target.value
-        })
-    }
-
-    handleSubmit = e => {
-        e.preventDefault();
-        const { contacts, name, number } = this.state;
-
-        if (name.trim() && number.trim() !== '') {
-            if (contacts.some(contact => contact.name === name)){
-                window.alert('Error: Contact already exists.')
-            } else{
-                // If contact doesn't exist, create a new contact object with the current timestamp as the unique 'id'
-                const newContact = {
-                    id: Date.now(),
-                    name: name,
-                    number: number,
-                }
-                // Add the new contacts array and reset the name field
-
-                this.setState(() => ({
-                    contacts: [...contacts, newContact],
-                    name: '',
-                    number: ''
-                }));
-            }
-            
-        } 
-    };
-    // Event handler for changes in the filter input field
-    handleFilterChange = e => {
-        this.setState({
-            filter: e.target.value
-        })
-    }
-
-    handleRemove = contactId => {
+        // Add the new contact to the contacts array and reset input fields
         this.setState(prevState => ({
-            contacts: prevState.contacts.filter(contact => contact.id !== contactId)
-        }))
-    
+          contacts: [...prevState.contacts, newContact],
+          name: '',
+          number: '',
+        }));
+      }
     }
+  };
 
-    render() {
-        // Extract the values from the state for ease of use
-        const { number, name, contacts, filter } = this.state;
+  handleFilterChange = e => {
+    // Update the 'filter' state when the filter input changes
+    this.setState({
+      filter: e.target.value,
+    });
+  };
 
-         // Filter the contacts based on the 'filter' state
-         const filteredContacts = contacts.filter(contact => contact.name.includes(filter))
+  handleRemove = contactId => {
+    // Remove a contact from the contacts array by its ID
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
 
-        const ContactForm = () => {
-            return(
-                <div>
-                    <MainContainer>
-                    <h1>Phonebook</h1>
-                    <ContactsTable>
-                {/* Input field for entering contact name */}
-                <ContactDetails>Name</ContactDetails>
-                <input
-                    type="text"
-                    name="name"
-                    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                    title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan."
-                    required
-                    value={name} 
-                    onChange={this.handleChange}
-                />
-                <ContactDetails>Phone Number</ContactDetails>
-                <input
-                    type="tel"
-                    name="number"
-                    pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                    title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                    required
-                    value={number}
-                    onChange={this.handleNumberChange}
-                /> <br></br>
-                <AddContactBttn onClick={this.handleSubmit}>Add contact</AddContactBttn>
-                </ContactsTable>
-                </MainContainer>
-                </div>
+  render() {
+    // Extract state values for easier use
+    const { number, name, contacts, filter } = this.state;
 
-            )
-        }
-        
-        const Filter = () => {
-            return(
-                <div>
-                    <ContactsContainer>
-                    <h2>Contacts</h2>
-                    <FindContact>Find contacts by name</FindContact>
-                <input
-                type="text"
-                placeholder="Search by name"
-                value={filter}
-                onChange={this.handleFilterChange}
-                
-                />
+    // Filter the contacts based on the 'filter' state
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.includes(filter)
+    );
 
-                {filteredContacts.length === 0 ? (
-                    // Display "No contacts" if there are no contacts in the 'contacts' array
-                    <h2>No contacts</h2>
-                ) : (
-                    // Display the list of contacts using the 'ul' and 'li' elements
-                    <ul>
-                        {filteredContacts.map(({name, number, id})=>(
-                            <li key={id}> {name}: {number} 
-                            
-                            <RemoveBttn type='button' onClick={() => this.handleRemove(id)}>Delete Contact</RemoveBttn>
-                            </li>  
-                        ))
-                        }
-                    </ul>
-                )}  
-                </ContactsContainer>
-                </div>
-            )
-        }
+    return (
+      <div>
+        {/* Main container */}
+        <MainContainer>
+          <h1>Phonebook</h1>
+          <ContactsTable>
+            <ContactDetails>Name</ContactDetails>
+            {/* Name input field */}
+            <input
+              type="text"
+              name="name"
+              required
+              value={name}
+              onChange={this.handleChange}
+            />
+            <ContactDetails>Phone Number</ContactDetails>
+            {/* Phone number input field */}
+            <input
+              type="tel"
+              name="number"
+              required
+              value={number}
+              onChange={this.handleNumberChange}
+            />
+            <br></br>
+            {/* Button to submit a new contact */}
+            <AddContactBttn onClick={this.handleSubmit}>
+              Add contact
+            </AddContactBttn>
+          </ContactsTable>
+        </MainContainer>
 
-        return(
-            <div>
-                <ContactForm></ContactForm>
-                <Filter></Filter>
-            </div>
-        )
-    }
+        {/* Contacts container */}
+        <ContactsContainer>
+          <h2>Contacts</h2>
+          <FindContact>Find contacts by name</FindContact>
+          {/* Filter input for searching contacts by name */}
+          <input
+            type="text"
+            placeholder="Search by name"
+            value={filter}
+            onChange={this.handleFilterChange}
+          />
+
+          {filteredContacts.length === 0 ? (
+            // Display "No contacts" if there are no contacts in the 'contacts' array
+            <h2>No contacts</h2>
+          ) : (
+            // Display the list of contacts using 'ul' and 'li' elements
+            <ul>
+              {filteredContacts.map(({ name, number, id }) => (
+                <li key={id}>
+                  {name}: {number}
+                  {/* Button to remove a contact */}
+                  <RemoveBttn
+                    type="button"
+                    onClick={() => this.handleRemove(id)}
+                  >
+                    Delete Contact
+                  </RemoveBttn>
+                </li>
+              ))}
+            </ul>
+          )}
+        </ContactsContainer>
+      </div>
+    );
+  }
 }
+
+export default PhoneBook;
